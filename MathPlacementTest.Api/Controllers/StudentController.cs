80df565f4d0cfb1a96cdde30f67d95bc9918035b
kirbyk <christian.allen.lee@gmail.com>
2021-04-14 09:46:02 -0500

    creating student controller skeleton

---
+++ MathPlacementTest.Api/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using MathPlacementTest.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MathPlacementTest.Api.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpPost]
+        [Route("Create")]
+        public StudentCreateView Create([FromForm] string student)
+        {
+            return default;
+        }
+    }
+
+}
