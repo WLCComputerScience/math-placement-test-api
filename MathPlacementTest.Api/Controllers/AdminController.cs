6a62baa4f888494855c2af59b88dc1ee9b3c08e5
Dylan Chirigotis <dylan.chirigotis@mail.wlc.edu>
2021-04-26 12:59:04 -0500

    Pushing Admin Controller

---
+++ MathPlacementTest.Api/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MathPlacementTest.Api.Controllers
+{
+    public class AdminController
+    {
+    }
+}
