f67561b6438d5e7b477029eb071e8c77609dd53f
lizardman301 <40240232+lizardman301@users.noreply.github.com>
2021-04-30 08:58:51 -0500

    Small fixes for last merge

--- MathPlacementTest.Data/MathTestDbContext.cs
+++ MathPlacementTest.Data/MathTestDbContext.cs
@@ -16,7 +16,6 @@ namespace MathPlacementTest.Data
         //public dbset<coursetaken> coursestaken { get; set; }
         //public dbset<question> questions { get; set; }
         //public dbset<studentanswer> studentanswers { get; set; }
-        public DbSet<Student> students { get; set; }
         //public dbset<testquestion> testquestions { get; set; }
         //public dbset<test> tests { get; set; }
 
