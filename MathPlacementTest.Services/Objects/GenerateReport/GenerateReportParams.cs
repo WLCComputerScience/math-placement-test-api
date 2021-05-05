b17807058435857c992b1b6721e31ce9a9283817
lizardman301 <40240232+lizardman301@users.noreply.github.com>
2021-05-05 09:47:08 -0500

    First Step of generating a csv file

---
+++ MathPlacementTest.Services/Objects/GenerateReport/GenerateReportParams.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathPlacementTest.Services.Objects
+{
+    public class GenerateReportParams
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
