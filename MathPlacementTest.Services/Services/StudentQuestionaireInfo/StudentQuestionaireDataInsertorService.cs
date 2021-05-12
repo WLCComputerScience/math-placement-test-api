df1034e412451871d11dfb4f85f53f9a6fce5c19
lizardman301 <40240232+lizardman301@users.noreply.github.com>
2021-05-12 10:36:22 -0500

    Fixed Multiple Entries being added for the same StudentId if StudentId exists in coursesTaken

--- MathPlacementTest.Services/Services/StudentQuestionaireInfo/StudentQuestionaireDataInsertorService.cs
+++ MathPlacementTest.Services/Services/StudentQuestionaireInfo/StudentQuestionaireDataInsertorService.cs
@@ -25,23 +25,34 @@ namespace MathPlacementTest.Services
             }
             try
             {
-                studentToUpdate.DesiredClass = studentQuestionaireInfoParams.DesiredClass;
-                studentToUpdate.MathInLastYear = studentQuestionaireInfoParams.HadMathInLastYear;
-                studentToUpdate.MostAdvancedClass = studentQuestionaireInfoParams.AdvancedCourse;
-                studentToUpdate.MostAdvancedClassGrade = studentQuestionaireInfoParams.GradeInAdvancedCourse;
-                _dbContext.SaveChanges();
-
-                foreach (var pastCourse in studentQuestionaireInfoParams.CoursesTaken)
+                var coursesTakenStudent = _dbContext.CoursesTaken.Where(s => s.StudentId == studentQuestionaireInfoParams.StudentId).FirstOrDefault();
+                if (coursesTakenStudent == null)
                 {
-                    var courseTaken = new CourseTaken()
-                    {
-                        PastCourseId = pastCourse.PastCourseId,
-                        StudentId = studentQuestionaireInfoParams.StudentId
-                    };
-                    _dbContext.CoursesTaken.Add(courseTaken);
+                    //If student exists in coursesTaken, we just ignore
+                    studentToUpdate.DesiredClass = studentQuestionaireInfoParams.DesiredClass;
+                    studentToUpdate.MathInLastYear = studentQuestionaireInfoParams.HadMathInLastYear;
+                    studentToUpdate.MostAdvancedClass = studentQuestionaireInfoParams.AdvancedCourse;
+                    studentToUpdate.MostAdvancedClassGrade = studentQuestionaireInfoParams.GradeInAdvancedCourse;
                     _dbContext.SaveChanges();
+
+                    // only update courses taken
+                    foreach (var pastCourse in studentQuestionaireInfoParams.CoursesTaken)
+                    {
+                        var courseTaken = new CourseTaken()
+                        {
+                            PastCourseId = pastCourse.PastCourseId,
+                            StudentId = studentQuestionaireInfoParams.StudentId
+                        };
+                        _dbContext.CoursesTaken.Add(courseTaken);
+                        _dbContext.SaveChanges();
+                    }
+                    return true;
                 }
-                return true;
+                else
+                {
+                    //Return false if student already exists in coursesTaken
+                    return false;
+                }
             }
             catch
             {
