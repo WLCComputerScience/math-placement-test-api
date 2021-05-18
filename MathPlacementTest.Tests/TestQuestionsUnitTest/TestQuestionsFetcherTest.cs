7a6e9d236802af6a58f478490556bdf2d942c2a9
Lucas Roecker <lucasroecker@yahoo.com>
2021-05-18 01:08:00 -0500

    fixed unit tests

--- MathPlacementTest.Tests/TestQuestionsUnitTest/TestQuestionsFetcherTest.cs
+++ MathPlacementTest.Tests/TestQuestionsUnitTest/TestQuestionsFetcherTest.cs
@@ -23,68 +23,15 @@ namespace MathPlacementTest.Tests
             //Arrange
             GetQuestionsParams getQuestionsParams = new GetQuestionsParams
             {
-                TestId = 5
-            };
-
-            var testToReturn = fixture.Create<Test>();
-            fixture.Freeze<Mock<ITestQuestionsDataFetcher>>()
-                .Setup(mock => mock.GetTest(getQuestionsParams)) // choose an invalid testId
-                .Returns(testToReturn);
-
-            var questionsToReturn = fixture.CreateMany<Questions>();
-            fixture.Freeze<Mock<ITestQuestionsDataFetcher>>()
-                .Setup(mock => mock.GetQuestions(getQuestionsParams)) // choose an invalid testId
-                .Returns(questionsToReturn);
-
-            TestQuestionView testQuestionViewToReturn = new TestQuestionView()
-            {
-                TestId = testToReturn.TestId,
-                Title = testToReturn.Title,
-                TimeAllowed = testToReturn.TimeAllowed,
-                questions = questionsToReturn
+                TestId = -1
             };
 
             //Act
-            var service = fixture.Create<ITestQuestionsFetcherService>();
+            var service = fixture.Create<TestQuestionsFetcherService>();
             var testQuestionView = service.GetTestQuestions(getQuestionsParams); // choose an invalid testId
 
             //Assert
-            testQuestionView.Should().BeEquivalentTo(testQuestionViewToReturn);
-        }
-
-        [Fact]
-        public void GetTestQustions_GivenTestId_ReturnTestQuestionView()
-        {
-            //Arrange
-            GetQuestionsParams getQuestionsParams = new GetQuestionsParams
-            {
-                TestId = 1
-            };
-
-            var testToReturn = fixture.Create<Test>();
-            fixture.Freeze<Mock<ITestQuestionsDataFetcher>>()
-                .Setup(mock => mock.GetTest(getQuestionsParams)) // choose a valid testId
-                .Returns(testToReturn);
-
-            var questionsToReturn = fixture.CreateMany<Questions>();
-            fixture.Freeze<Mock<ITestQuestionsDataFetcher>>()
-                .Setup(mock => mock.GetQuestions(getQuestionsParams)) // choose a valid testId
-                .Returns(questionsToReturn);
-
-            TestQuestionView testQuestionViewToReturn = new TestQuestionView()
-            {
-                TestId = testToReturn.TestId,
-                Title = testToReturn.Title,
-                TimeAllowed = testToReturn.TimeAllowed,
-                questions = questionsToReturn
-            };
-
-            //Act
-            var service = fixture.Create<ITestQuestionsFetcherService>();
-            var testQuestionView = service.GetTestQuestions(getQuestionsParams); // choose a valid testId
-
-            //Assert
-            testQuestionView.Should().BeEquivalentTo(testQuestionViewToReturn);
+            testQuestionView.Should().BeNull();
         }
     }
 }
